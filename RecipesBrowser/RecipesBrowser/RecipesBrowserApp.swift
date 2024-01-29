228c0aa859a5d9e6744e73c9fb4df44c426f7c89
Pavel Tikhonov <pash3r@gmail.com>
2024-01-29 17:31:10 -0500

    initial commit

---
+++ RecipesBrowser/RecipesBrowser/RecipesBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesBrowserApp.swift
+//  RecipesBrowser
+//
+//  Created by Pavel Tikhonov on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
